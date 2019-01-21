495056c0beaecfbd55892c4b265230eaf96c9b90
Yasuaki Uechi <y@uechi.io>
2019-01-21 18:19:45 +0900

    wrap code with async func

--- examples/github.js
+++ examples/github.js
@@ -1,13 +1,17 @@
 const { Muffled, bearerAuth } = require('muffled')
 
-/**
- * GitHub API
- */
-const GitHubAPI = new Muffled('api.github.com')
+async function main() {
+  /**
+   * GitHub API
+   */
+  const GitHubAPI = new Muffled('api.github.com')
 
-// Inject "Authorization: Bearer <token>" header into every request
-GitHubAPI.use(bearerAuth(process.env.GITHUB_TOKEN))
+  // Inject "Authorization: Bearer <token>" header into every request
+  GitHubAPI.use(bearerAuth(process.env.GITHUB_TOKEN))
 
-// This will fetch resource from `https://api.github.com/user/repos`
-const result = await GitHubAPI.user.repos()
-console.log(result)
+  // This will fetch resource from `https://api.github.com/user/repos`
+  const result = await GitHubAPI.user.repos()
+  console.log(result)
+}
+
+main().catch(err => console.error(err))
--- examples/spotify.js
+++ examples/spotify.js
@@ -1,16 +1,20 @@
 const { Muffled, bearerAuth } = require('muffled')
 
-/**
- * Spotify API
- */
-const SpotifyAPI = new Muffled('api.spotify.com')
+async function main() {
+  /**
+   * Spotify API
+   */
+  const SpotifyAPI = new Muffled('api.spotify.com')
 
-// Inject "Authorization: Bearer <token>" header into every request
-SpotifyAPI.use(bearerAuth(process.env.SPOTIFY_TOKEN))
+  // Inject "Authorization: Bearer <token>" header into every request
+  SpotifyAPI.use(bearerAuth(process.env.SPOTIFY_TOKEN))
 
-// This will fetch resource from `https://api.spotify.com/v1/search`
-const result = await SpotifyAPI.v1.search({
-  q: 'roadhouse blues',
-  type: 'album,track',
-})
-console.log(result)
+  // This will fetch resource from `https://api.spotify.com/v1/search`
+  const result = await SpotifyAPI.v1.search({
+    q: 'roadhouse blues',
+    type: 'album,track',
+  })
+  console.log(result)
+}
+
+main().catch(err => console.error(err))
