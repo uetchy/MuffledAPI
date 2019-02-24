560f39be57625214d70df304613d702b180a5de4
Yasuaki Uechi <y@uechi.io>
2019-02-24 14:53:05 +0900

    add nature remo example

---
+++ examples/nature-remo.js
@@ -0,0 +1,26 @@
+const { Muffled, bearerAuth } = require('muffled')
+
+async function main() {
+  /**
+   * NatureRemo API
+   */
+  const NatureRemo = new Muffled('api.nature.global/1')
+
+  // Inject "Authorization: Bearer <token>" header into every request
+  NatureRemo.use(bearerAuth(process.env.NATURE_REMO_TOKEN))
+
+  // This will fetch resource from `https://api.nature.global/1/devices`
+  const devices = await NatureRemo.devices()
+  console.log(devices)
+
+  const {
+    newest_events: { te, hu },
+    name,
+  } = devices[0]
+
+  console.log(`[${name}]`)
+  console.log(`Temperature: ${te.val}`)
+  console.log(`Humidity: ${hu.val}`)
+}
+
+main().catch((err) => console.error(err))
